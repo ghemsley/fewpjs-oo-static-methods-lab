f7deb2f01ad97cdb52f77a9441668afc9aa4caad
ghemsley <ghemsley@protonmail.ch>
2021-05-24 22:10:13 -0500

    commit tree 892d3e43dcbf6b376c4ff7767a035b06c9474a2e

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    const firstLetter = string[0].toUpperCase()
+    const rest = string.slice(1)
+    return firstLetter + rest
+  }
+
+  static sanitize(string) {
+    let regex = new RegExp(/[^\da-z '-]/gi)
+    let newString = string.replace(regex, '')
+    return newString
+  }
+
+  static titleize(string) {
+    const list = new Set(['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'])
+    const words = string.split(' ')
+    let newString = []
+    for (const word of words) {
+      if (!list.has(word)) {
+        newString.push(this.capitalize(word))
+      } else {
+        newString.push(word)
+      }
+    }
+    return Formatter.capitalize(newString.join(' '))
+  }
 }
